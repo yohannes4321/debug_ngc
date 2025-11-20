5800d37d1ea1d8ab39057bcd740b84c0512fffd8
Yohannes Alemu <alemuyohannes960@gmail.com>
2025-11-20 23:52:07 +0300

    d

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ class Config:
     batch_size = 5
     vocab_size = 11710 # data vocab size + special tokens = 11706 + 4
     n_heads = 2
-    n_layers = 1
+    n_layers = 6
     dropout_rate = 0.1
     eta = 0.0001
     exp_dir = "exp" 
     pos_learnable = True
     optim_type = "adam"
-    num_iter = 5
+    num_iter = 20
     wub = 0.01
     wlb = -0.01
     tau_m = 10.
